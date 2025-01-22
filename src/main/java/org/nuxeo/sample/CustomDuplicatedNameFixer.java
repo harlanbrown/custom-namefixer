c097ca49971b71004efd081ac42a885ea6939c95
harlan.brown@hyland.com <harlan.brown@hyland.com>
2025-01-22 11:32:51 -0600

    first

---
+++ src/main/java/org/nuxeo/sample/CustomDuplicatedNameFixer.java
@@ -0,0 +1,33 @@
+package org.nuxeo.sample;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.nuxeo.ecm.core.api.event.CoreEventConstants;
+import org.nuxeo.ecm.core.event.Event;
+import org.nuxeo.ecm.core.event.EventListener;
+import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
+import org.nuxeo.ecm.core.api.DocumentExistsException;
+
+/**
+ * @author Stephane Lacoin at Nuxeo (aka matic)
+ */
+public class CustomDuplicatedNameFixer implements EventListener {
+    private static final Logger log = LogManager.getLogger(CustomDuplicatedNameFixer.class);
+    @Override
+    public void handleEvent(Event event) {
+        log.info("HI");
+        DocumentEventContext context = (DocumentEventContext) event.getContext();
+        Boolean destinationExists = (Boolean) context.getProperty(CoreEventConstants.DESTINATION_EXISTS);
+        if (!destinationExists) {
+            return;
+        } else {
+            log.info("nope");
+            throw new DocumentExistsException("nope");
+        } 
+
+//        String name = (String) context.getProperty(CoreEventConstants.DESTINATION_NAME);
+//        name += '.' + String.valueOf(System.currentTimeMillis());
+//        context.setProperty(CoreEventConstants.DESTINATION_NAME, name);
+    }
+
+}
